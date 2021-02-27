e317c71b43de10a45a9a6b071d428ddf88f71816
Frieder Schrempf <dev@fris.de>
2021-02-27 21:58:16 +0100

    Remove unused files

--- conf/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Default settings for the extendpage plugin
- *
- * @author Frieder Schrempf <dev@fris.de>
- */
-
-//$conf['fixme']    = 'FIXME';
-
--- conf/metadata.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Options for the extendpage plugin
- *
- * @author Frieder Schrempf <dev@fris.de>
- */
-
-//$meta['fixme'] = array('string');
-
